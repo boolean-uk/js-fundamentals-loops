244c5c6a99938623cd74d95ba36f209cbb6e10f1
Samuel Odigie <samodigie98@gmail.com>
2022-09-14 09:12:17 +0100

    done

--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -2,7 +2,18 @@ const numsZeroToThree = []
 const numsFiveToTen = []
 const evenNums = []
 const countdown = []
-
+for (let i = 0; i < 4; i++) {
+  numsZeroToThree.push(i)
+}
+for (let i = 5; i < 11; i++) {
+  numsFiveToTen.push(i)
+}
+for (let i = 0; i <= 6; i += 2) {
+  evenNums.push(i)
+}
+for (let i = 3; i >= 0; i--) {
+  countdown.push(i)
+}
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
 
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
