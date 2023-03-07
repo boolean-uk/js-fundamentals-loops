3c276fc0711e2c744f2925f04fedc27fe0acb1a0
Lee Kelly <leeowenkelly@gmail.com>
2023-03-07 16:11:07 +0000

    core exercises completed

--- src/for-loop-and-arrays.js
+++ src/for-loop-and-arrays.js
@@ -6,26 +6,36 @@ let word = '' // eslint-disable-line prefer-const
 // TODO: Add code below this line to make the tests pass
 
 // 1. Use a for loop to set the sum variable to the sum of all the values in nums
-for (let i = 0; i < nums.length; i++) sum += nums[i]
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i]
+}
 console.log(sum)
 
 // 2. Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
-for (let j = 0; j < nums.length; j++) doubledNums.push(nums[j] * 2)
+for (let j = 0; j < nums.length; j++) {
+  doubledNums.push(nums[j] * 2)
+}
 console.log(doubledNums)
 
 // 3. Use a for loop to set word equal to all the letters in the letters array
-for (let n = 0; n < letters.length; n++) word += letters[n]
+for (let n = 0; n < letters.length; n++) {
+  word += letters[n]
+}
 console.log(word)
 
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
-for (let p = 1; p < nums.length; p += 2) everySecondNum.push(nums[p])
+for (let p = 1; p < nums.length; p += 2) {
+  everySecondNum.push(nums[p])
+}
 console.log(everySecondNum)
 
 // 5. Use a for loop to populate numsReversed with the numbers from nums in reverse order
 const numsReversed = []
-for (let m = nums.length - 1; m > -1; m--) numsReversed.push(nums[m])
+for (let m = nums.length - 1; m > -1; m--) {
+  numsReversed.push(nums[m])
+}
 console.log(numsReversed)
 
 // do not change below this line
--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -4,19 +4,27 @@ const evenNums = []
 const countdown = []
 
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-for (let i = 0; i < 4; i++) numsZeroToThree.push(i)
+for (let i = 0; i < 4; i++) {
+  numsZeroToThree.push(i)
+}
 console.log(numsZeroToThree)
 
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-for (let j = 5; j < 11; j++) numsFiveToTen.push(j)
+for (let j = 5; j < 11; j++) {
+  numsFiveToTen.push(j)
+}
 console.log(numsFiveToTen)
 
 // TODO: 3. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
-for (m = 0; m < 7; m += 2) evenNums.push(m)
+for (let k = 0; k < 7; k += 2) {
+  evenNums.push(k)
+}
 console.log(evenNums)
 
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-for (n = 3; n > -1; n--) countdown.push(n)
+for (let n = 3; n > -1; n--) {
+  countdown.push(n)
+}
 console.log(countdown)
 
 // do not change below this line
