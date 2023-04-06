d9bbddb84189531a4df8e04bf12c12e144b0a825
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-04-06 17:39:03 +0300

    new push

--- src/for-loop-and-arrays.js
+++ src/for-loop-and-arrays.js
@@ -29,9 +29,15 @@ for (let i = 0; i < letters.length; i++) {
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
 
-for (let i = 1; i < nums.length; i += 2) {
-  everySecondNum.push(nums[i])
+// for (let i = 1; i < nums.length; i += 2) {
+//   everySecondNum.push(nums[i])
+// }
+for (let i = 0; i < nums.length; i++) {
+  if (i % 2 !== 0) {
+    everySecondNum.push(nums[i])
+  }
 }
+console.log(everySecondNum)
 // console.log(everySecondNum)
 // 5. Use a for loop to populate numsReversed with the numbers from nums in reverse order
 const numsReversed = []
