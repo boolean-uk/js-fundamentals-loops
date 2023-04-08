1546810f52db73185151db860605a2dc192bc958
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-08 08:12:39 +0200

    finished question 4 nested-loop

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -48,6 +48,18 @@ for (let d = START; d < END + 1; d++) {
 // console.log(nestedTwo)
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = START; i < END + 1; i++) {
+  const arrayA = []
+  for (let j = START; j <= i; j++) {
+    const arrayB = []
+    for (let n = START; n <= j; n++) {
+      arrayB.push(i)
+    }
+    arrayA.push(arrayB)
+  }
+  deepOne.push(arrayA)
+}
+// console.log(deepOne)
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
