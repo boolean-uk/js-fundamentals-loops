1ffc00d6cf366b73f975d98421b14ffb6cdcda06
Martyna <martyna.marszalek@talan.com>
2023-07-19 14:02:26 +0200

    Extensions done

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -10,6 +10,7 @@ const deepThree = []
 
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
+for (let i = START; i <= END; i++) simpleOne.push(i)
 
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
@@ -19,18 +20,55 @@ const deepThree = []
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
-
+for (let i = START; i <= END; i++) {
+  const help = []
+  for (let j = START; j <= i; j++) {
+    help.push(i)
+  }
+  nestedOne.push(help)
+}
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+for (let i = START; i <= END; i++) {
+  const help = []
+  for (let j = i; j >= 1; j--) {
+    help.push(j)
+  }
+  nestedTwo.push(help)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
-
+for (let i = START; i <= END; i++) {
+  const help = []
+  deepOne.push([])
+  for (let j = START; j <= i; j++) {
+    help.push(i)
+    deepOne[i - 1].push([...help])
+  }
+}
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
-
+for (let i = START; i <= END; i++) {
+  const help = []
+  deepTwo.push([])
+  for (let j = START; j <= i; j++) {
+    help.push(j)
+    deepTwo[i - 1].push([...help])
+  }
+}
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+for (let i = START; i <= END; i++) {
+  let sum = 0
+  const help = []
+  for (let j = START; j <= i; j++) {
+    sum += j * j
+    const avg = sum / j
+    help.push([avg])
+  }
+  deepThree.push(help)
+}
 
 module.exports = {
   START,
