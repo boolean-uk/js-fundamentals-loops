74ecd574a0765715386d8149f22ee0f4bbd965ee
Ali Keskin <cmdalikeskin@gmail.com>
2023-08-12 00:56:54 +0200

    test ext 1

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -8,7 +8,7 @@ const deepOne = []
 const deepTwo = []
 const deepThree = []
 
-console.log('test')
+// console.log('test')
 
 
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
@@ -16,10 +16,11 @@ console.log('test')
 
 for (let i = START; i <= END; i++) {
   simpleOne.push(i)
-  
+
 }
 
-console.log(simpleOne)
+
+
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
 // for (let i = 5; i < 7; i++) {
@@ -28,18 +29,58 @@ console.log(simpleOne)
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+for (let i = START; i <= END; i++) {
+
+  const innerArray = [];
+  for (let j = 0; j < i; j++) {
+    innerArray.push(i);
+  }
+  nestedOne.push(innerArray);
+}
+
+
+
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
 
+for (let i = START; i <= END; i++) {
+
+  const innerArray = [];
+  for (let j = 0; j < i; j++) {
+    innerArray.push(i);
+    innerArray.push(1);
+  }
+  nestedTwo.push(innerArray);
+}
+
+
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+
+for (let i = START; i <= END; i++) {
+
+  const innerArray = [];
+  for (let j = 0; j < i; j++) {
+    innerArray.push(i);
+  }
+  deepOne.push(innerArray);
+}
+
+console.log(deepOne)
+
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+
+
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+
+
+
 
 module.exports = {
   START,
