54c85b378a202a68ef7e49b9fb74d1cf90ef07a9
tom <141278466+tomlukeprogarm@users.noreply.github.com>
2023-09-06 15:03:51 +0100

    Update for-loop-basic.js

--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -18,6 +18,7 @@ for(let i = 0; i <= 6; i += 2){
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
 for (let i = 3; i> = 0; i--){
   countdown.push(i)
+}
 // do not change below this line
 module.exports = {
   a: numsZeroToThree,
