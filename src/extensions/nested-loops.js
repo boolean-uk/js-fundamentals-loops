5aa1a62318eb8af961caa32ec2f0ffb632559551
Rebecca Noy <rebecca.noy@gmx.de>
2023-09-06 17:15:31 +0200

    Solved remaining 2 excercises in extension

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -18,15 +18,11 @@ const deepThree = []
 // }
 
 for (let i = 1; i <= 10; i++) {
-  simpleOne.push(i)
-}
-console.log(simpleOne)
+  simpleOne.push(i)}
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2], [3,3,3], [4,4,4,4]...]
-
-console.log('Question 2')
 for (let i = 1; i <= 10; i++) {
 
   // create a temporary array we then can push
@@ -35,30 +31,20 @@ for (let i = 1; i <= 10; i++) {
     tempArray.push(i)
   }
   // push this tempArray to nestedOne
-  nestedOne.push(tempArray)
-  console.log(`current state of nestedOne in iteration ${i}:`, nestedOne)
-}
-console.log(`final state`, nestedOne)
+  nestedOne.push(tempArray)}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],[3,2,1]...]
 
-console.log('Question 3')
-
 for (let i = 1; i <= 10; i++) {
   const newArr = []
   for (let j = i; j > 0; j--) {
-    newArr.push(j)
-    console.log(j)
+    newArr.push(j)  
   }
-  nestedTwo.push(newArr)
-}
-console.log(nestedTwo)
+  nestedTwo.push(newArr)}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],[[3],[3,3],[3,3,3]], ...]
-
-console.log('Question 4')
 
 for (let i = 1; i <= 10; i++) {
   // create a temporary array we then can push
@@ -66,23 +52,42 @@ for (let i = 1; i <= 10; i++) {
   for (let j = 1; j <= i; j++) {
     const tempArray1 = []
     for (let k = 0; k < j; k++) {
-      tempArray1.push(i)
-      console.log(k)
-      console.log(i)
+      tempArray1.push(i)        
     }
     tempArray2.push(tempArray1)
   }
   // push this tempArray to nestedOne
-  deepOne.push(tempArray2)
-  console.log(`current state of deepOne in iteration ${i}:`, deepOne)
-}
-console.log(`final state`, deepOne)
+  deepOne.push(tempArray2)}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+for (let i = 1; i <=10; i++) {
+  let tempArray1 = []
+    for (let j= 1; j <= i; j++) {
+      let tempArray2 = []
+        for (let k=1; k <= j; k++) {
+          tempArray2.push(k)
+        }
+        tempArray1.push(tempArray2)
+    }
+    deepTwo.push(tempArray1)
+}
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
+// 1^2 + 2^2 / 2
 //       [[1],[[1],[2.5]],...]
+for (let i = 1; i <=10; i++) {
+  let tempArray1 = []
+    for (let j= 1; j <= i; j++) {
+      let tempSum = 0
+        for (let k=1; k <= j; k++) {
+          tempSum += k ** 2
+        }
+        tempArray1.push([tempSum / j])
+    }
+    deepThree.push(tempArray1)
+}
 
 module.exports = {
   START,
