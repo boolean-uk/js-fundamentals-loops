e573901459efefab558c3aba9268a29fab447e10
irlydo <fokum8@gmail.com>
2023-09-06 16:22:26 +0100

    finished

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -11,26 +11,90 @@ const deepThree = []
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
 
-// HINT: in the below loop, the statements and block of code needs to be changed
-// HINT: in the below loop, the var i represents the loop index
-// for (let i = 5; i < 7; i++) {
-// Your code here
-// }
+for (let i = 1; i < 11; i++) {
+  simpleOne.push(i)
+}
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
 
+for (let i = START; i <= END; i++) {
+  const array = []
+
+  for (let n = 1; n <= i; n++) {
+    array.push(i)
+  }
+  nestedOne.push(array)
+}
+console.log('Answer two', nestedOne)
+
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+
+for (let i = START; i <= END; i++) {
+  const array = []
+
+  for (let n = i; n >= 1; n--) {
+    array.push(n)
+  }
+  nestedTwo.push(array)
+}
+console.log('Answer Three', nestedTwo)
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
 
+for (let i = START; i <= END; i++) {
+  const array = []
+
+  for (let n = 1; n <= i; n++) {
+    const array2 = []
+    for (let p = 1; p <= n; p++) {
+      array2.push(i)
+    }
+    array.push(array2)
+  }
+  deepOne.push(array)
+}
+
+console.log('Answer Four', deepOne)
+
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+for (let i = START; i <= END; i++) {
+  const array = []
+
+  for (let n = 1; n <= i; n++) {
+    const array2 = []
+
+    for (let p = 1; p <= n; p++) {
+      array2.push(p)
+    }
+    array.push(array2)
+  }
+  deepTwo.push(array)
+}
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+
+for (let i = START; i <= END; i++) {
+  const array = []
+
+  for (let n = 1; n <= i; n++) {
+    const array2 = []
+    let sum = 0
+
+    for (let p = 1; p <= n; p++) {
+      sum += p ** 2
+      array2.push(p)
+    }
+    array.push([sum / array2.length])
+  }
+
+  deepThree.push(array)
+}
 
 module.exports = {
   START,
--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -4,13 +4,44 @@ const evenNums = []
 const countdown = []
 
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
+for (let i = 0; i < 4; i++) {
+  numsZeroToThree.push(i)
+}
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
+for (let i = 5; i < 11; i++) {
+  numsFiveToTen.push(i)
+}
 
 // TODO: 3. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
 
+for (let i = 0; i < 1; i++) {
+  evenNums.push(i)
+}
+for (let i = 2; i < 3; i++) {
+  evenNums.push(i)
+}
+for (let i = 4; i < 5; i++) {
+  evenNums.push(i)
+}
+for (let i = 6; i < 7; i++) {
+  evenNums.push(i)
+}
+
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
 
+for (let i = 3; i < 4; i++) {
+  countdown.push(i)
+}
+for (let i = 2; i < 3; i++) {
+  countdown.push(i)
+}
+console.log(countdown)
+for (let i = 1; i < 2; i++) {
+  countdown.push(i)
+}
+for (let i = 0; i < 1; i++) {
+  countdown.push(i)
+}
 // do not change below this line
 module.exports = {
   a: numsZeroToThree,
