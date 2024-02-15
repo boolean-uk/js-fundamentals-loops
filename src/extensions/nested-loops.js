2a4bfc101696dbeecfb26862e0f0b294441dc9df
Najem Hamo <najem.hamo@gmail.com>
2024-02-15 14:53:28 +0100

    Core & Extensions

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -16,21 +16,73 @@ const deepThree = []
 // for (let i = 5; i < 7; i++) {
 // Your code here
 // }
+for (let i = START; i <= END; i++) {
+  simpleOne.push(i)
+}
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+for (let i = START; i <= END; i++) {
+  const arr = []
+  for (let j = 0; j < i; j++) {
+    arr.push(i)
+  }
+  nestedOne.push(arr)
+}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+for (let i = START; i <= END; i++) {
+  const arr = []
+  for (let j = i; j > 0; j--) {
+    arr.push(j)
+  }
+  nestedTwo.push(arr)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = START; i <= END; i++) {
+  const arr = []
+  for (let j = 1; j <= i; j++) {
+    const innerArr = []
+    for (let k = 0; k < j; k++) {
+      innerArr.push(i)
+    }
+    arr.push(innerArr)
+  }
+  deepOne.push(arr)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+for (let i = START; i <= END; i++) {
+  const innerArray = []
+  for (let j = 1; j <= i; j++) {
+    const subArray = []
+    for (let k = 1; k <= j; k++) {
+      subArray.push(k)
+    }
+    innerArray.push(subArray)
+  }
+  deepTwo.push(innerArray)
+}
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+for (let i = START; i <= END; i++) {
+  const innerArray = []
+  for (let j = 1; j <= i; j++) {
+    let sumOfSquares = 0
+    for (let k = 1; k <= j; k++) {
+      sumOfSquares += k * k
+    }
+    const average = sumOfSquares / j
+    innerArray.push([average])
+  }
+  deepThree.push(innerArray)
+}
 
 module.exports = {
   START,
