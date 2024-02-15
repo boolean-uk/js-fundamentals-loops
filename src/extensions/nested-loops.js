1892f6a1fc30573bcec347f8ba4d899de16ad36e
llllllll-l <vincent.werelius@gmail.com>
2024-02-15 15:04:20 +0100

    Core, Extensions and Advanced

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -10,6 +10,9 @@ const deepThree = []
 
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
+for (let i = START; i <= END; i++) {
+  simpleOne.push(i)
+}
 
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
@@ -19,19 +22,69 @@ const deepThree = []
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+for (let i = START; i <= END; i++) {
+  const innerArr = []
+  for (let j = 1; j <= i; j++) {
+    innerArr.push(i)
+  }
+  nestedOne.push(innerArr)
+}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
 
+for (let i = START; i <= END; i++) {
+  const innerArr = []
+  for (let j = i; j >= 1; j--) {
+    innerArr.push(j)
+  }
+  nestedTwo.push(innerArr)
+}
+
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = START; i <= END; i++) {
+  const innerArr = []
+  for (let j = 1; j <= i; j++) {
+    const subArr = []
+    for (let k = 1; k <= j; k++) {
+      subArr.push(i)
+    }
+    innerArr.push(subArr)
+  }
+  deepOne.push(innerArr)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
+for (let i = START; i <= END; i++) {
+  const innerArr = []
+  for (let j = 1; j <= i; j++) {
+    const subArr = []
+    for (let k = 1; k <= j; k++) {
+      subArr.push(k)
+    }
+    innerArr.push(subArr)
+  }
+  deepTwo.push(innerArr)
+}
 
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
-
+for (let i = START; i <= END; i++) {
+  const innerArray = []
+  for (let j = 1; j <= i; j++) {
+    const subArray = []
+    let sumOfSquares = 0
+    for (let k = 1; k <= j; k++) {
+      subArray.push(i)
+      sumOfSquares += k * k
+    }
+    innerArray.push([sumOfSquares / j])
+  }
+  deepThree.push(innerArray)
+}
+console.log(deepThree)
 module.exports = {
   START,
   END,
