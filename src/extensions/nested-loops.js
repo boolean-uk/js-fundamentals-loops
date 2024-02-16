695f13fa79648966dc25ab8934c8ddb6d179313e
kristianverduin <kristianverduin@gmail.com>
2024-02-16 14:17:19 +0100

    Finished everything

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -11,6 +11,10 @@ const deepThree = []
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
 
+for (let i = 1; i <= 10; i++) {
+  simpleOne.push(i)
+}
+
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
 // for (let i = 5; i < 7; i++) {
@@ -20,17 +24,67 @@ const deepThree = []
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
 
+for (let i = 1; i <= 10; i++) {
+  const tempArray = []
+  for (let j = 0; j < i; j++) {
+    tempArray.push(i)
+  }
+  nestedOne.push(tempArray)
+}
+
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+
+for (let i = 1; i <= 10; i++) {
+  const tempArray = []
+  for (let j = i; j > 0; j--) {
+    tempArray.push(j)
+  }
+  nestedTwo.push(tempArray)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
 
+for (let i = 1; i <= 10; i++) {
+  const tempArray = []
+  for (let j = 1; j <= i; j++) {
+    const tempArray2 = []
+    for (let k = 1; k <= j; k++) {
+      tempArray2.push(i)
+    }
+    tempArray.push(tempArray2)
+  }
+  deepOne.push(tempArray)
+}
+
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+for (let i = 1; i <= 10; i++) {
+  const tempArray = []
+  for (let j = 1; j <= i; j++) {
+    const tempArray2 = []
+    for (let k = 1; k <= j; k++) {
+      tempArray2.push(k)
+    }
+    tempArray.push(tempArray2)
+  }
+  deepTwo.push(tempArray)
+}
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+
+for (let i = 1; i <= 10; i++) {
+  const tempArray = []
+  let result = 0
+  for (let j = 1; j <= i; j++) {
+    result += j ** 2
+    tempArray.push([result / j])
+  }
+  deepThree.push(tempArray)
+}
 
 module.exports = {
   START,
