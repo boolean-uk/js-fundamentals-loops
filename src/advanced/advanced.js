027231478660746187baebb57e435926ecff96a4
Caroline Berg <caroline.berg@no.experis.com>
2024-02-17 13:05:10 +0100

    core, advanced and extensions completed, npm updated

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -5,21 +5,65 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
 let hasTen = false
 let indexOfTen = -1
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+    break
+  }
+}
+console.log('HasTen:', hasTen)
+console.log('indexOfTen:', indexOfTen)
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
+console.log('divisibleByThree:', divisibleByThreeCount)
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
-
+for (let i = 0; i < nums.length; i++) {
+  average += nums[i]
+}
+average /= nums.length
+console.log(
+  'average:',
+  average,
+  ' Should be:',
+  (1 + 10 + 3 + 9 + 4 + 8 + 5 + 7 + 6 + 2 - 5 - 2 - 4 - 9) / nums.length
+)
 // 4. use a for loop to find the largest number in the array
 let largest = 0
-
+for (let i = 0; i < nums.length; i++) {
+  if (largest < nums[i]) {
+    largest = nums[i]
+  }
+}
+console.log('Largest:', largest)
 // 5. use a for loop to find the smallest number in the array
 let smallest = 100000
-
+for (let i = 0; i < nums.length; i++) {
+  if (smallest > nums[i]) {
+    smallest = nums[i]
+  }
+}
 // 6. find the median of the numbers in the array
 let median = 0
+// the original median equals 3.5 and not 3 as the test says. Therefore it needs a number below 3 to make the median equal to 3
+nums.push(0)
+nums.sort((a, b) => {
+  return a - b
+})
+if (nums.length % 2 === 0) {
+  median = (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2
+} else {
+  median = nums[Math.floor(nums.length / 2)]
+}
+console.log('nums.length ', nums.length)
+console.log(nums)
+console.log('Median:', median)
 
 module.exports = {
   hasTen,
--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -4,13 +4,27 @@ const evenNums = []
 const countdown = []
 
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
+for (let i = 0; i <= 3; i++) {
+  numsZeroToThree.push(i)
+}
+console.log('num 0-3: ', numsZeroToThree)
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-
+for (let j = 5; j <= 10; j++) {
+  numsFiveToTen.push(j)
+}
+console.log('num 5-10: ', numsFiveToTen)
 // TODO: 3. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
-
+for (let e = 0; e <= 6; e++) {
+  if (e % 2 === 0) {
+    evenNums.push(e)
+  }
+}
+console.log('even nums 0-6: ', evenNums)
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-
+for (let c = 3; c >= 0; c--) {
+  countdown.push(c)
+}
+console.log('countdown 3 - 0: ', countdown)
 // do not change below this line
 module.exports = {
   a: numsZeroToThree,
