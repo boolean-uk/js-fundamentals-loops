637f540e08d0fa35c8fee8d61bcf4b90472ee50c
Kristian <70452674+Solido7@users.noreply.github.com>
2024-02-19 11:47:34 +0100

    Small fix

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -12,9 +12,6 @@ for (let i = 0; i < nums.length; i++) {
   }
 }
 
-[1, 2, 3, 4, 5, 6, 7, 8]
-[1, 2, 3, 4, 5, 6, 7]
-
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
 for (num of nums) {
