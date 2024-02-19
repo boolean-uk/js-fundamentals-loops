866de21ab6b95f1afcb86c66d099ad2e3fbf705d
Philip Morud <philip.morud@no.experis.com>
2024-02-19 15:17:01 +0100

    Advanced

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -3,23 +3,61 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // 1. Use a for loop to set the variable hasTen to true if the array contains the value 10
 // note: use a break statement to exit the loop early if the value is found
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
-const hasTen = false
-const indexOfTen = -1
+let hasTen = false
+let indexOfTen = -1
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+  }
+}
 
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
-const divisibleByThreeCount = 0
+let divisibleByThreeCount = 0
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
 
 // 3. use a for loop to find the average of the numbers in the array
-const average = 0
+let average = 0
+for (let i = 0; i < nums.length; i++) {
+  average += nums[i]
+}
+average = average / nums.length
 
 // 4. use a for loop to find the largest number in the array
-const largest = 0
+let largest = 0
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > largest) {
+    largest = nums[i]
+  }
+}
 
 // 5. use a for loop to find the smallest number in the array
-const smallest = 100000
+let smallest = 100000
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i]
+  }
+}
 
 // 6. find the median of the numbers in the array
-const median = 0
+let median = 0
+const sortedNums = nums.sort(function (a, b) {
+  return a > b ? 1 : -1
+})
+if (sortedNums.length % 2 === 0) {
+  median =
+    (sortedNums[sortedNums.length / 2 - 1] +
+      sortedNums[sortedNums.length / 2]) /
+    2
+} else {
+  median = sortedNums[(sortedNums.length - 1) / 2]
+}
 
 module.exports = {
   hasTen,
