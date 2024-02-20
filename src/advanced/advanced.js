70249525c8404b30e1d2d7d3c55c5c5160b88ad8
Periklis <perik76@gmail.com>
2024-02-20 18:15:49 +0200

    Advanced almost done. Extensions 1-2 done

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -3,23 +3,54 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // 1. Use a for loop to set the variable hasTen to true if the array contains the value 10
 // note: use a break statement to exit the loop early if the value is found
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
-const hasTen = false
-const indexOfTen = -1
+let hasTen = false
+let indexOfTen = -1
+for (let i = 0; i < nums.length; i++){
+  if (nums[i] === 10) {
+    hasTen = true;
+    indexOfTen = i;
+    break;
+  }
+}
+// console.log(hasTen, indexOfTen)
 
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
-const divisibleByThreeCount = 0
+let divisibleByThreeCount = 0
+for (let i = 0; i < nums.length; i++){
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
+// console.log(divisibleByThreeCount);
 
 // 3. use a for loop to find the average of the numbers in the array
-const average = 0
+let average = 0
+let sum = 0
+for (let i = 0; i < nums.length; i++) {
+  sum = sum + nums[i]
+}
+average = sum / nums.length
+// console.log(average)
 
 // 4. use a for loop to find the largest number in the array
-const largest = 0
-
+let largest = 0
+for (let i = 0; i < nums.length; i++){
+  if (nums[i] > largest) {
+    largest = nums[i]
+  }
+}
+// console.log(largest);
 // 5. use a for loop to find the smallest number in the array
-const smallest = 100000
-
+let smallest = 100000
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i]
+  }
+}
+console.log(smallest);
 // 6. find the median of the numbers in the array
 const median = 0
+
 
 module.exports = {
   hasTen,
--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -16,12 +16,26 @@ const deepThree = []
 // for (let i = 5; i < 7; i++) {
 // Your code here
 // }
+for (let i = START; i <= END; i++) {
+  simpleOne.push(i)
+}
+// console.log(simpleOne)
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
 
+for (let i = 1; i <= 10; i++) {
+  let loopIndex = []
+  for (j = 0; j < i; j++) {
+    loopIndex.push(i)
+  }
+  nestedOne.push(loopIndex)
+}
+// console.log(nestedOne)
+
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
