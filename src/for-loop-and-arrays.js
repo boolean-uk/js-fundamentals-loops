debacc4efbdf8df1d35b8f93cd403a1ceb7e2a19
Kyle Vann <kyle.vann1996@icloud.com>
2024-02-20 16:37:13 +0000

    tasks done, no extension

--- src/for-loop-and-arrays.js
+++ src/for-loop-and-arrays.js
@@ -6,18 +6,39 @@ let word = '' // eslint-disable-line prefer-const
 // TODO: Add code below this line to make the tests pass
 
 // 1. Use a for loop to set the sum variable to the sum of all the values in nums
+for (const element of nums) {
+  console.log(element)
+
+  sum = sum + element
+}
+console.log('this is the sum', sum)
 
 // 2. Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
-
+for (const element of nums) {
+  console.log(element)
+  doubledNums.push(element * 2)
+}
+console.log(doubledNums)
 // 3. Use a for loop to set word variable equal to all the letters in the letters array combined as a single string
-
+for (const element of letters) {
+  word = word + element
+}
+console.log(word)
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
-
+for (let i = 1; i < 8; i += 2) {
+  console.log(nums[i])
+  everySecondNum.push(nums[i])
+}
+console.log(everySecondNum)
 // 5. Use a for loop to populate numsReversed with the numbers from nums in reverse order
 const numsReversed = []
-
+for (let i = 8; i > -1; i--) {
+  console.log(i)
+  numsReversed.push(nums[i])
+}
+console.log(numsReversed)
 // do not change below this line
 module.exports = {
   a: sum,
--- src/for-loop-basic.js
+++ src/for-loop-basic.js
@@ -4,13 +4,30 @@ const evenNums = []
 const countdown = []
 
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
+for (let i = 0; i < 4; i++) {
+  console.log(i)
+  numsZeroToThree.push(i)
+}
+console.log(numsZeroToThree)
 
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-
+for (let i = 5; i < 11; i++) {
+  console.log(i)
+  numsFiveToTen.push(i)
+}
+console.log(numsFiveToTen)
 // TODO: 3. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
-
+for (let i = 0; i < 7; i += 2) {
+  console.log(i)
+  evenNums.push(i)
+}
+console.log(evenNums)
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-
+for (let i = 3; i > -1; i--) {
+  console.log(i)
+  countdown.push(i)
+}
+console.log(countdown)
 // do not change below this line
 module.exports = {
   a: numsZeroToThree,
