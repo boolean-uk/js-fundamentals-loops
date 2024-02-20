6be31aa6db7a1d7d3b5f129507cb0c9a666454d0
Your Name <your_email@address>
2024-02-20 18:18:37 +0000

    exercise passed

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -5,21 +5,48 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
 let hasTen = false
 let indexOfTen = -1
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+    break
+  }
+}
 
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
+for (let i = 0; i < nums.length; i++) {
+  divisibleByThreeCount += nums[i] % 3 === 0
+}
 
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
+for (let i = 0; i < nums.length; i++) {
+  average += nums[i] / nums.length
+}
 
 // 4. use a for loop to find the largest number in the array
 let largest = 0
+for (let i = 0; i < nums.length; i++) {
+  largest = Math.max(...nums)
+}
 
 // 5. use a for loop to find the smallest number in the array
 let smallest = 100000
+for (let i = 0; i < nums.length; i++) {
+  smallest = Math.min(...nums)
+}
 
 // 6. find the median of the numbers in the array
 let median = 0
+nums.sort((a, b) => a - b)
+if (nums.length % 2 !== 0) {
+  const middle = Math.floor(nums.length / 2)
+  median = nums[middle]
+} else {
+  const middle = Math.floor(nums.length / 2)
+  median = (nums[middle] + nums[middle - 1]) / 2
+}
 
 module.exports = {
   hasTen,
