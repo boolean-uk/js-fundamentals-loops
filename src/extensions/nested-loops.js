3c0df42a3abc5c4ba4592b2e2f7830a3b245fff1
Oneallane <oneallane2808@gmail.com>
2024-02-20 09:32:16 +0100

    Task Completed

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -16,21 +16,73 @@ const deepThree = []
 // for (let i = 5; i < 7; i++) {
 // Your code here
 // }
+for (let i = START; i < END + 1; i++) {
+  simpleOne.push(i)
+}
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+for (let i = START; i < END + 1; i++) {
+  const innerArray = []
+  for (let j = 0; j < i; j++) {
+    innerArray.push(i)
+  }
+  nestedOne.push(nestedTwo)
+}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+for (let i = START; i < END + 1; i++) {
+  const innerArray = []
+  for (let j = i; j >= 1; j--) {
+    innerArray.push(i)
+  }
+  nestedTwo.push(innerArray)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = START; i < END + 1; i++) {
+  const innerArray = []
+  for (let j = 0; j <= i; j++) {
+    const innestArray = []
+    for (let k = 0; k < j; k++) {
+      innestArray.push(i)
+    }
+    innerArray.push(innestArray)
+  }
+  deepOne.push(innerArray)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
+for (let i = START; i <= END; i++) {
+  const innerArray = []
+  for (let j = 1; j <= i; j++) {
+    const innestArray = []
+    for (let k = 1; k <= j; k++) {
+      innestArray.push(i === 1 ? 1 : k === j ? i : 1)
+    }
+    innerArray.push(innestArray)
+  }
+  deepTwo.push(innerArray)
+}
 
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+for (let i = 1; i <= END; i++) {
+  const innerArray = []
+  for (let j = 1; j <= i; j++) {
+    const innestArray = []
+    let sum = 0
+    for (let k = 1; k <= j; k++) {
+      innestArray.push(i === 1 ? 1 : k === j ? i : 1)
+      sum += (i === 1 ? 1 : k === j ? i : 1) ** 2
+    }
+    innerArray.push(Math.sqrt(sum / innestArray.length))
+  }
+  deepThree.push(innerArray)
+}
 
 module.exports = {
   START,
