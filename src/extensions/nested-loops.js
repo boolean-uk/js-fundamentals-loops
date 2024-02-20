709d0897265d07356490d909483c5277a11b0388
Tim Zoltie <t.zoltie@gmail.com>
2024-02-20 18:04:24 +0000

    for-loop-and-arrays

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -10,7 +10,10 @@ const deepThree = []
 
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
-
+for (let i = 1; i < 11; i++) {
+  simpleOne.push(i)
+  console.log(simpleOne)
+}
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
 // for (let i = 5; i < 7; i++) {
