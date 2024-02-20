c40430cec944a45d0cdf6b0ceb9445d4c91bd5bf
Periklis <perik76@gmail.com>
2024-02-20 23:16:07 +0200

    Advanced -1. Nested -2

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -5,18 +5,18 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
 let hasTen = false
 let indexOfTen = -1
-for (let i = 0; i < nums.length; i++){
+for (let i = 0; i < nums.length; i++) {
   if (nums[i] === 10) {
-    hasTen = true;
-    indexOfTen = i;
-    break;
+    hasTen = true
+    indexOfTen = i
+    break
   }
 }
 // console.log(hasTen, indexOfTen)
 
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
-for (let i = 0; i < nums.length; i++){
+for (let i = 0; i < nums.length; i++) {
   if (nums[i] % 3 === 0) {
     divisibleByThreeCount++
   }
@@ -34,7 +34,7 @@ average = sum / nums.length
 
 // 4. use a for loop to find the largest number in the array
 let largest = 0
-for (let i = 0; i < nums.length; i++){
+for (let i = 0; i < nums.length; i++) {
   if (nums[i] > largest) {
     largest = nums[i]
   }
@@ -47,10 +47,9 @@ for (let i = 0; i < nums.length; i++) {
     smallest = nums[i]
   }
 }
-console.log(smallest);
+console.log(smallest)
 // 6. find the median of the numbers in the array
 const median = 0
-
 
 module.exports = {
   hasTen,
