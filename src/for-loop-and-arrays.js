04e27c158aa50927487462f2dc12f8ac1ea923a7
Farshad <f.bagdeli13@gmail.com>
2024-02-21 09:42:07 +0100

    Basic Trains

--- src/for-loop-and-arrays.js
+++ src/for-loop-and-arrays.js
@@ -6,17 +6,36 @@ let word = '' // eslint-disable-line prefer-const
 // TODO: Add code below this line to make the tests pass
 
 // 1. Use a for loop to set the sum variable to the sum of all the values in nums
-
+for (i of nums) {
+sum = i + sum
+}
 // 2. Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
+for(let i = 0;i < nums.length ; i++){
+  doubledNums[i] = (nums[i] * 2)
+}
 
 // 3. Use a for loop to set word variable equal to all the letters in the letters array combined as a single string
 
+for(let i =0;i<letters.length ;i++){
+word = word+letters[i]
+}
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
+let b = 0
+for(let i = 1;i < nums.length ; i+=2 ) {
+everySecondNum[b] = nums[i]
+b++
+}
+
 
 // 5. Use a for loop to populate numsReversed with the numbers from nums in reverse order
+b = 0
 const numsReversed = []
+for(let i = nums.length -1;i>-1;i--){
+  numsReversed[b] = nums[i]
+  b++
+}
 
 // do not change below this line
 module.exports = {
