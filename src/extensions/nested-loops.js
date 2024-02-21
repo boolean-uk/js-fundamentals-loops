a86f49096c66f489a004dc66e04183c7a6c9451b
Axel Hansson <axel.co.hansson@gmail.com>
2024-02-21 10:58:01 +0100

    Advanced complete

--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -10,6 +10,9 @@ const deepThree = []
 
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
+for (let i = START; i <= END; i++) {
+  simpleOne.push(i)
+}
 
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
@@ -20,17 +23,78 @@ const deepThree = []
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
 
+for (let i = START; i <= END; i++) {
+  const arr = []
+
+  for (let j = 0; j < i; j++) {
+    arr.push(i)
+  }
+
+  nestedOne.push(arr)
+}
+
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+
+for (let i = START; i <= END; i++) {
+  const arr = []
+
+  for (let j = i; j > 0; j--) {
+    arr.push(j)
+  }
+
+  nestedTwo.push(arr)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
 
+for (let i = START; i <= END; i++) {
+  const arr1 = []
+
+  for (let j = 1; j <= i; j++) {
+    const arr2 = []
+    for (let k = 0; k < j; k++) {
+      arr2.push(i)
+    }
+    arr1.push(arr2)
+  }
+
+  deepOne.push(arr1)
+}
+
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
 
+for (let i = START; i <= END; i++) {
+  const arr1 = []
+  for (let j = 1; j <= i; j++) {
+    const arr2 = []
+    for (let k = 1; k <= j; k++) {
+      arr2.push(k)
+    }
+    arr1.push(arr2)
+  }
+  deepTwo.push(arr1)
+}
+
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+
+for (let i = START; i <= END; i++) {
+  const arr1 = []
+  for (let j = 1; j <= i; j++) {
+    const arr2 = []
+    let total = 0
+
+    for (let k = 1; k <= j; k++) {
+      total += k * k
+    }
+    arr2.push(total / j)
+    arr1.push(arr2)
+  }
+  deepThree.push(arr1)
+}
 
 module.exports = {
   START,
